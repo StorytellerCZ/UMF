2f856b2d09bbacc2fa65ef17eb14b1fe0750f9e0
AdwinTrave <jxd3455@rit.edu>
2014-11-11 14:36:49 -0500

    Autoload and constants file fix

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -96,7 +96,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('account/Authorization', 'account/Authentication');
 
 
 /*
@@ -124,7 +124,7 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('account/ssl', 'language', 'url');
 
 
 /*
@@ -140,7 +140,7 @@ $autoload['helper'] = array();
 |
 */
 
-$autoload['config'] = array();
+$autoload['config'] = array('umf/account');
 
 
 /*
@@ -156,7 +156,7 @@ $autoload['config'] = array();
 |
 */
 
-$autoload['language'] = array();
+$autoload['language'] = array('general');
 
 
 /*
@@ -173,7 +173,7 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('first_model' => 'first');
 */
 
-$autoload['model'] = array();
+$autoload['model'] = array('account/Account_model');
 
 
 /* End of file autoload.php */
--- application/config/constants.php
+++ application/config/constants.php
@@ -121,5 +121,46 @@ define('EXIT_DATABASE', 8); // database error
 define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
+/*
+* Define root resources folder name for js/css/img files
+*/
+define('RES_DIR', 'resource');
+/*
+* Portable PHP password hashing framework
+* http://www.openwall.com/phpass/
+*/
+define('PHPASS_HASH_STRENGTH', 8);
+define('PHPASS_HASH_PORTABLE', FALSE);
+
+/*
+|--------------------------------------------------------------------------
+| Mahana Messaging Constants
+|--------------------------------------------------------------------------
+*/
+// message statuses
+define('MSG_STATUS_UNREAD', 0);
+define('MSG_STATUS_READ', 1);
+define('MSG_STATUS_ARCHIVED', 2);
+// priority
+define('PRIORITY_LOW', 1);
+define('PRIORITY_NORMAL', 2);
+define('PRIORITY_HIGH', 3);
+define('PRIORITY_URGENT', 4);
+// status return message codes
+define('MSG_SUCCESS', 0);
+define('MSG_ERR_GENERAL', 1);
+define('MSG_ERR_INVALID_USER_ID', 2);
+define('MSG_ERR_INVALID_MSG_ID', 3);
+define('MSG_ERR_INVALID_THREAD_ID', 4);
+define('MSG_ERR_INVALID_STATUS_ID', 5);
+define('MSG_ERR_INVALID_SENDER_ID', 6);
+define('MSG_ERR_INVALID_RECIPIENTS', 7);
+define('MSG_MESSAGE_SENT', 8);
+define('MSG_STATUS_UPDATE', 9);
+define('MSG_PARTICIPANT_ADDED', 10);
+define('MSG_ERR_PARTICIPANT_EXISTS', 11);
+define('MSG_ERR_PARTICIPANT_NONSYSTEM', 12);
+define('MSG_PARTICIPANT_REMOVED', 13);
+
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
